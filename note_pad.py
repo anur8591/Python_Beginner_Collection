042e890eebcd061364b77c354899c0c4204efb3e
Anurag Pal <palanurag595@gmail.com>
2026-06-28 00:02:01 +0530

    Notepad_first-complete_verson

--- note_pad.py
+++ note_pad.py
@@ -18,4 +18,21 @@ def save_file():
             file.write(text.get(1.0, tk.END))
             messagebox.showinfo("Info", "File saved successfully!")
 
+root = tk.Tk()
+root.title("simple notepad")
+root.geometry("800x600")
 
+menu = tk.Menu(root )
+root.config(menu=menu)
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+text = tk.Text(root, wrap=tk.WORD, font=('Helvetica', 12), fg = 'black', bg = 'white')
+text.pack(expand=tk.YES, fill=tk.BOTH)
+
+root.mainloop()
