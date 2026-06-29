b401ff38879a0063eea4fd696a184c41134385b1
Anurag Pal <palanurag595@gmail.com>
2026-06-29 23:58:06 +0530

    image_slideshow program

--- image_slideshow.py
+++ image_slideshow.py
@@ -20,8 +20,27 @@ image_paths = [
     r"C:\Users\Anurag\OneDrive\Desktop\python_mini_task\image\P90468919_highRes_munich-ger-17-june-2.jpg",
 ]
 #resize the image to 1080x1080
-image_size=(1080,1080)
-images = [Image.open(path).resize(image_size) for path in image_paths]
+# image_size=(1080, 720)
+images = [Image.open(path) for path in image_paths]
 # converting into photoimageobject to use in tkinter 
 photo_images = [ImageTk.PhotoImage(image) for image in images]
 
+label = tk.Label(root)
+label.pack()
+
+def update_image():
+    for photo_image in photo_images:
+        label.config(image=photo_image)
+        label.update()
+        time.sleep(3)
+
+slideshow = cycle(photo_images)
+
+def start_slideshow():
+    for _ in range(len(image_paths)):
+        update_image()
+
+play_button = tk.Button(root, text = "play", command=start_slideshow)
+play_button.pack()
+
+root.mainloop()
