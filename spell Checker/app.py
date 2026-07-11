7d5ce43846295993a96a65be9c4f2f3ee5a8807a
Anurag Pal <palanurag595@gmail.com>
2026-07-11 23:59:35 +0530

    spell checker

---
+++ spell Checker/app.py
@@ -0,0 +1,31 @@
+from spellchecker import SpellChecker
+
+class SpallCheckerApp:
+    def __init__(self):
+        self.spell = SpellChecker()
+
+    def correct_text(self, text):
+        words = text.split()
+        corrected_word = []
+
+        for word in words:
+            corrected_word = self.spell.correction(word)
+            if corrected_word != word.lower():
+                print(f'correction "{word}" to "{corrected_word}"')
+                corrected_word.append(corrected_word)
+
+        return ' '.join(corrected_word)
+    
+    def run(self):
+        print("\n---Spell_Checker---")
+
+        while True:
+            text = input("Enter text to check(or type 'exit' to quit): ")
+            if text.lower() == 'exit':
+                print('closing the program....')
+                break
+            corrected_text = self.correct_text(text)
+            print(f'Corrected Text : {self.correct_text}')
+
+if __name__ == "__main__":
+    SpallCheckerApp().run()
